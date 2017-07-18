b9d808842739adc109da5aeb3c841a0deefb6830
avi <hi@avi.im>
2017-07-18 13:14:02 +0530

    initial import

---
+++ frago/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  frago
+//
+//  Created by avi on 18/07/17.
+//  Copyright © 2017 avi.im. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
