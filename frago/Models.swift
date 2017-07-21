0e0decf61972fad856d2caea3c328f0464ffe7e8
avi <hi@avi.im>
2017-07-21 18:24:11 +0530

    Add `initDB` to create tables if they not exist

--- frago/Models.swift
+++ frago/Models.swift
@@ -10,6 +10,15 @@ import SQLite
 
 class StocksDB {
 
+    private let stocks = Table("stocks")
+    private let name = Expression<String>("name")
+    private let createdOn = Expression<Int64>("created_on")
+    private let updatedOn = Expression<Int64>("updated_on")
+    private let targetLowPrice = Expression<Double>("target_low_price")
+    private let targetHighPrice = Expression<Double>("target_high_price")
+    private let currentPrice = Expression<Double>("current_price")
+    private let intialPrice = Expression<Double>("intial_price")
+
     static let instance = StocksDB()
     private let db: Connection?
 
@@ -25,10 +34,33 @@ class StocksDB {
                 atPath: path, withIntermediateDirectories: true, attributes: nil
             )
             db = try Connection("\(path)/db.sqlite3")
+            initDB()
         } catch {
             // TODO: Handle this error correctly
             // probbaly exit the app
             fatalError("Failed to connect to DB")
         }
+    }
+
+    // creates tables if they not exist
+    func initDB() {
+        do {
+            try db!.run(stocks.create(ifNotExists: true) { table in
+                table.column(name, primaryKey: true)
+                table.column(createdOn)
+                table.column(updatedOn)
+                table.column(targetLowPrice)
+                table.column(targetHighPrice)
+                table.column(currentPrice)
+                table.column(intialPrice)
+            })
+        } catch {
+            fatalError("Failed to create tables")
+        }
+    }
+
+    // returns all stocks
+    func getStocks() {
+
     }
 }
